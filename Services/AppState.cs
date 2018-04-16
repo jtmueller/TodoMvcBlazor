cfd417e71d886b78e10fe6b778ff137d42cba27e
Joel Mueller <joel.mueller@gmail.com>
2018-04-16 00:40:19 -0500

    Adding localStorage support

--- Services/AppState.cs
+++ Services/AppState.cs
@@ -2,11 +2,15 @@ using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
+using Microsoft.AspNetCore.Blazor;
+using Microsoft.AspNetCore.Blazor.Browser.Interop;
 
 namespace TodoMvcBlazor.Services
 {
     public class AppState
     {
+        public AppState() => LoadTodos();
+
         private readonly List<Todo> _todos = new List<Todo>();
         public IReadOnlyList<Todo> Todos => _todos;
 
@@ -20,7 +24,17 @@ namespace TodoMvcBlazor.Services
             var todo = new Todo { Text = text };
             todo.PropertyChanged += OnItemChanged;
             _todos.Add(todo);
-            NotifyStateChanged();
+            StateChanged();
+        }
+
+        public void AddRange(IEnumerable<Todo> todos)
+        {
+            foreach (var todo in todos)
+            {
+                todo.PropertyChanged += OnItemChanged;
+                _todos.Add(todo);
+            }
+            StateChanged();
         }
 
         public void RemoveTodo(Todo todo)
@@ -28,14 +42,14 @@ namespace TodoMvcBlazor.Services
             if (_todos.Remove(todo))
             {
                 todo.PropertyChanged -= OnItemChanged;
-                NotifyStateChanged();
+                StateChanged();
             }
         }
 
         public void ClearCompleted()
         {
             _todos.RemoveAll(t => t.Completed);
-            NotifyStateChanged();
+            StateChanged();
         }
 
         public void ToggleAll(bool isChecked)
@@ -44,11 +58,37 @@ namespace TodoMvcBlazor.Services
             {
                 td.Completed = isChecked;
             }
-            NotifyStateChanged();
+            StateChanged();
         }
 
-        private void NotifyStateChanged() => OnChange?.Invoke();
+        private void LoadTodos()
+        {
+            // registered in index.html
+            var todoStr = RegisteredFunction.Invoke<string>("loadTodos");
+            if (string.IsNullOrEmpty(todoStr))
+                return;
 
-        private void OnItemChanged(object sender, PropertyChangedEventArgs e) => NotifyStateChanged();
+            var todos = JsonUtil.Deserialize<Todo[]>(todoStr);
+            if (todos.Length > 0)
+            {
+                AddRange(todos);
+            }
+            OnChange?.Invoke();
+        }
+        
+        private void SaveTodos()
+        {
+            var todoStr = JsonUtil.Serialize(Todos);
+            // registered in index.html
+            RegisteredFunction.Invoke<object>("saveTodos", todoStr);
+        }
+
+        private void StateChanged()
+        {
+            OnChange?.Invoke();
+            SaveTodos();
+        }
+
+        private void OnItemChanged(object sender, PropertyChangedEventArgs e) => StateChanged();
     }
 }
